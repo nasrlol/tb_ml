4b625a811039ded1671bf6a2dd8919356403a5d9
nasr <nsrddyn@gmail.com>
2026-04-23 22:33:01 +0200

    feature(model): getting nan value when training model. but the core idea is working were training something. just very wrong
    i dont think that the issue necesarrely lies with the model training
    logic because i approached that properly but the csv decorder could be
    the issue like i mentioned before

--- source/tb_ml/tb_ml.c
+++ source/tb_ml/tb_ml.c
@@ -27,6 +27,7 @@ btree_node_traverse(btree_node *node) {
     if(is_nil_btree_node(node)) {
         return;
     }
+
     for (s32 index = 0; index < node->key_count; ++index) {
         _log("Key index: %d, Payload: %p\n", index, node->payload_per_key[index]);
     }
@@ -51,11 +52,25 @@ btree_traverse_inorder(btree *bt) {
 
 #if 1
 
-internal ts *
+internal ts
 normalize_training_data_btree(btree *bt) {
 
-    unused(bt);
-    return NULL;
+    ts sample = {0};
+
+    assert_msg(bt->root != NULL, "null root");
+
+    btree_node *current_node = bt->root;
+    s32 kc = current_node->key_count;
+
+    for (s32 index = 0; index < kc; ++index) {
+        for (s32 index = 0; index < kc; ++index) {
+            sample.w += current_node->keys[index].header_index;
+            sample.y += current_node->keys[index].row_index;
+        }
+        current_node = current_node->children[index];
+    }
+
+    return sample;
 }
 
 #endif
@@ -63,7 +78,7 @@ normalize_training_data_btree(btree *bt) {
 
 internal f32
 predict(f32 b1, f32 b2) {
-    return b1 + (b2 * log(b1)) + M_E;
+    return b1 + (b2 * log(b1)) + m_e;
 }
 
 
@@ -72,7 +87,7 @@ predict(f32 b1, f32 b2) {
 *  btree order
 **/
 internal model
-train(ts *s, s32 learning_rate, f32 expected, f32 order) {
+train(ts s, s32 learning_rate, f32 expected, f32 order) {
 
     model m = {0};
     //- cost function
@@ -80,7 +95,7 @@ train(ts *s, s32 learning_rate, f32 expected, f32 order) {
 
     for (s32 epoch = 0; epoch < learning_rate; ++epoch)
     {
-        m.value = predict(s->y, s->y);
+        m.value = predict(s.y, s.y);
         delta = (1/ (2 * order)) * ((expected - delta) * (expected - delta));
     }
 
@@ -93,34 +108,49 @@ int main(int count, char **value) {
 
     mem_arena *global_arena = arena_create(GiB(1));
 
-    const char *path = PushStruct(global_arena, char);
-    if(count > 2) path = value[2];
-
-    path = "./test/data.csv";
-
-    unused(count);
-    unused(value);
+    const char *path = "./test/data.csv";
+    if(count > 2) {
+        path = value[2];
+    }
 
 
     string8 buffer = file_load(global_arena, path);
 
+    _log("path :: %s\n",path);
+    // _logs8(buffer);
+    _log("buffer size: %llu\n",buffer.size);
+
     csv_table      *table      = PushStructZero(global_arena, csv_table);
     csv_token_list *token_list = PushStructZero(global_arena, csv_token_list);
 
-    token_list->start_token = &nil_csv_token;
-    token_list->end_token   = &nil_csv_token;
+
+    {
+#if 1
+        token_list->start_token = &nil_csv_token;
+        token_list->end_token   = &nil_csv_token;
+#else
+        token_list->start_token = NULL;
+        token_list->end_token   = NULL;
+#endif
+    }
+
+    assert_msg(buffer.size != 0, "[BEFORE INITIALIZATION] buffer size is 0\n");
 
     csv_token *tokens = tokenize_csv(buffer, global_arena, table, token_list);
-    assert_msg(tokens != NULL, "tokens are null");
+    assert_msg(tokens != NULL, "tokens are null\n");
+
+    _log("First token: %p\n", token_list->start_token);
+    _log("Token list is nil: %d\n", is_nil_csv_token(token_list->start_token));
 
     btree *bt = btree_parse_csv(global_arena, token_list, table);
 
-    ts *sample = normalize_training_data_btree(bt);
+    assert_msg(bt != NULL, "btree creation failed\n");
+    ts sample = normalize_training_data_btree(bt);
     arena_clear(global_arena);
 
-#if 0
     model m = train(sample, epochs, 0.5, BTREE_ORDER);
-#endif
+
+    _log("trained value :: %f", m.value);
 
     btree_node_traverse(bt->root);
 
